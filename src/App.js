c91f076ce9848788abb11aaf23a647125190a74c
Ina Nuri Yohari <inanuriyohari@gmail.com>
2020-02-22 13:08:35 +0700

    gmap embed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+
 function App() {
     return (
         <Router>
---
+++ src/components/Gmap.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Gmap() {
+    return (
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.4397892586207!2d106.81584655123575!3d-6.2055741954855925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f41db2ce466f%3A0xd4c9f73690dbaef8!2sSudirman%20Park%20Apartment!5e0!3m2!1sen!2sid!4v1582351155830!5m2!1sen!2sid" width="600" height="450" frameborder="0" style={{border:0, allowfullscreen:""}} />
+        </div>
+    )
+}
