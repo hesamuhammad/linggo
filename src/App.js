48622aa05523c4c62b2223e6c82eae330c08f12e
Hesa Muhammad <hesamilan@gmail.com>
2020-02-28 13:59:52 +0700

    Add translator

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TranslatorNDA from "./pages/Terms/TranslatorNDA";
 import Signin from "./pages/Users/Signin";
 import Signup from "./pages/Users/Signup";
 import Ourteam from "./pages/Team/Ourteam";
+import Translator from "./pages/Translator/TranslatorPage";
 
 function App() {
     return (
@@ -55,6 +56,9 @@ function App() {
                 </Route>
                 <Route exact path="/terms-of-service/translator-nda/">
                     <TranslatorNDA />
+                </Route>
+                <Route exact path="/translators">
+                    <Translator />
                 </Route>
             </Switch>
             <Footer />
