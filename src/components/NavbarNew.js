be9bc021301f9017f5deb00c046b16cf44340d3d
rhfauzi <rhfauzi7@gmail.com>
2020-03-05 19:19:58 +0700

    add id user setelah login ke nav

--- src/components/NavbarNew.js
+++ src/components/NavbarNew.js
@@ -11,140 +11,122 @@ import { loggedOut } from "../actions/users";
 import "./NavbarNew.css";
 
 const NavbarNew = props => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-    const handleLogout = () => {
-        Swal.fire({
-            text: "Are You Sure to logout ?",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Sure ?",
-            timer: 2000
-        }).then(result => {
-            if (result.value) {
-                Swal.fire(
-                    "Success!",
-                    "Your account has been logged out!",
-                    "Succeded"
-                );
-                localStorage.clear();
-                props.loggedOut();
-                props.history.push("/");
-            }
-        });
-    };
+  const handleLogout = () => {
+    Swal.fire({
+      text: "Are You Sure to logout ?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sure ?",
+      timer: 2000
+    }).then(result => {
+      if (result.value) {
+        Swal.fire("Success!", "Your account has been logged out!", "Succeded");
+        localStorage.clear();
+        props.loggedOut();
+        props.history.push("/");
+      }
+    });
+  };
 
-    const token = localStorage.getItem("token");
-    // let decode = "";
-    if (token) {
-        let decode = jwt(token);
-    }
+  const token = localStorage.getItem("token");
+  // let decode = "";
+  if (token) {
+    let decode = jwt(token);
+  }
+  const JWT = require("jsonwebtoken");
+  const decoded = JWT.verify(token, "INISECRET");
+  const idUsers = decoded._id;
 
-    console.log(props);
-
-    return (
-        <Navbar
-            collapseOnSelect
-            expand="lg"
-            // bg="dark"
-            // variant="dark"
-            sticky="top"
-        >
-            <Container>
-                <Navbar.Brand as={Link} to="/" onClick={toggle}>
-                    <div style={{ maxWidth: "18vh" }}>
-                        <img
-                            src={Image}
-                            alt="linggo"
-                            style={{ width: "100%" }}
-                        />
-                    </div>
-                </Navbar.Brand>
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      // bg="dark"
+      // variant="dark"
+      sticky="top"
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="/" onClick={toggle}>
+          <div style={{ maxWidth: "18vh" }}>
+            <img src={Image} alt="linggo" style={{ width: "100%" }} />
+          </div>
+        </Navbar.Brand>
+        <Fragment>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" isOpen={isOpen}>
+            <Nav className="mr-auto">
+              {props.users.isLogin ? (
                 <Fragment>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" isOpen={isOpen}>
-                        <Nav className="mr-auto">
-                            {props.users.isLogin ? (
-                                <Fragment>
-                                    {/* <Nav.Link
-                                        className="MenuNav"
-                                        href="/service"
-                                    >
-                                        SERVICES
-                                    </Nav.Link>
-                                    <Nav.Link href="/pricing">PRICING</Nav.Link>
-                                    <Nav.Link href="/aboutus">
-                                        ABOUT LINGGO
-                                    </Nav.Link>
-                                    <Nav.Link href="/contactus">
-                                        CONTACT US
-                                    </Nav.Link> */}
-                                    <Nav.Link href="/profile/">
-                                        MY PROFILE
-                                    </Nav.Link>
-                                    <Nav.Link href="/project">
-                                        MY PROJECT
-                                    </Nav.Link>
-                                </Fragment>
-                            ) : (
-                                <Fragment>
-                                    <Nav.Link href="/service">
-                                        SERVICES
-                                    </Nav.Link>
-                                    <Nav.Link href="/pricing">PRICING</Nav.Link>
-                                    <Nav.Link href="/aboutus">
-                                        ABOUT LINGGO
-                                    </Nav.Link>
-                                    <Nav.Link href="/contactus">
-                                        CONTACT US
-                                    </Nav.Link>
-                                </Fragment>
-                            )}
-                        </Nav>
-                        <Nav>
-                            <Nav.Link>
-                                <Drawer />
-                            </Nav.Link>
-                        </Nav>
-                        {console.log(props.isLogin, "islog")}
-                        {props.users.isLogin ? (
-                            <Nav>
-                                <Nav.Link eventKey={2} onClick={handleLogout}>
-                                    SIGN OUT
-                                </Nav.Link>
-                            </Nav>
-                        ) : (
-                            <Nav>
-                                <Nav.Link eventKey={2} href="/signin">
-                                    SIGN IN
-                                </Nav.Link>
-                            </Nav>
-                        )}
-                    </Navbar.Collapse>
+                  {/* <Nav.Link
+                    className="MenuNav"
+                    href="/service"
+                >
+                    SERVICES
+                </Nav.Link>
+                <Nav.Link href="/pricing">PRICING</Nav.Link>
+                <Nav.Link href="/aboutus">
+                    ABOUT LINGGO
+                </Nav.Link>
+                <Nav.Link href="/contactus">
+                    CONTACT US
+                </Nav.Link> */}
+                  <Nav.Link href={`/profile/${idUsers}`}>MY PROFILE</Nav.Link>
+                  <Nav.Link href={`/project-users/${idUsers}`}>
+                    MY PROJECT
+                  </Nav.Link>
                 </Fragment>
-            </Container>
-        </Navbar>
-    );
+              ) : (
+                <Fragment>
+                  <Nav.Link href="/service">SERVICES</Nav.Link>
+                  <Nav.Link href="/pricing">PRICING</Nav.Link>
+                  <Nav.Link href="/aboutus">ABOUT LINGGO</Nav.Link>
+                  <Nav.Link href="/contactus">CONTACT US</Nav.Link>
+                </Fragment>
+              )}
+            </Nav>
+            <Nav>
+              <Nav.Link>
+                <Drawer />
+              </Nav.Link>
+            </Nav>
+            {props.users.isLogin ? (
+              <Nav>
+                <Nav.Link eventKey={2} onClick={handleLogout}>
+                  SIGN OUT
+                </Nav.Link>
+              </Nav>
+            ) : (
+              <Nav>
+                <Nav.Link eventKey={2} href="/signin">
+                  SIGN IN
+                </Nav.Link>
+              </Nav>
+            )}
+          </Navbar.Collapse>
+        </Fragment>
+      </Container>
+    </Navbar>
+  );
 };
 
 const mapStateToProps = state => {
-    console.log(state.users);
-
-    return {
-        users: state.users
-    };
+  return {
+    users: state.users
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        loggedOut: () => dispatch(loggedOut())
-    };
+  return {
+    loggedOut: () => dispatch(loggedOut())
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(withRouter(NavbarNew));
