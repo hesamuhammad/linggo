83e210a3af4c5d2cfdc40f816d5a0f4eef92c2bd
rhfauzi <rhfauzi7@gmail.com>
2020-03-05 13:14:53 +0700

    push word count

--- src/pages/Checkword/CheckPriceFromText.js
+++ src/pages/Checkword/CheckPriceFromText.js
@@ -1,12 +1,47 @@
-import React, { useState } from "react";
-import { Container, Form, Col, Button, Modal } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Form, Col, Button, Modal, Alert } from "react-bootstrap";
+
+import { Formik } from "formik";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { wordCount } from "../../actions/wordcount";
 
 function CheckPriceFromText(props) {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const handleShows = () => setShow(true);
+
+  useEffect(() => {
+    // const wordcount = JSON.parse(localStorage.getItem("wordcount"));
+    // props.wordCount(wordcount);
+    // console.log("wordcount", wordcount);
+
+    const timer = setTimeout(() => {
+      localStorage.removeItem("wordcount");
+      localStorage.removeItem("textWord");
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const getTextWord = () => {
+    const textWord = JSON.parse(localStorage.getItem("textWord"));
+    return textWord.textProject;
+  };
+
+  const harga = option => {
+    const wordcount = JSON.parse(localStorage.getItem("wordcount"));
+
+    let harga = wordcount.hargaTotal;
+    let jumlahKata = wordcount.jumlahKata;
+    // return harga.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+    // return wordcount.jumlahKata;
+
+    if (option === "kata") {
+    } else if (option === "harga") {
+    } else {
+      return "";
+    }
+  };
 
   const mystyle = {
     boxCount: {
@@ -15,7 +50,15 @@ function CheckPriceFromText(props) {
       margin: "50px 0px",
       border: "1px solid #e6e6e6"
     },
-    h2: { color: "#595959", margin: "0 0 15px", fontWeight: "700" }
+    h2: { color: "#595959", margin: "0 0 15px", fontWeight: "700" },
+    btnCekWord: {
+      backgroundColor: "#3072ac",
+      borderColor: "#2e6da4",
+      padding: "20px",
+      fontSize: "17px",
+      marginBottom: "20px",
+      width: "100%"
+    }
   };
 
   const cekLogin = () => {
@@ -26,48 +69,115 @@ function CheckPriceFromText(props) {
     }
   };
 
-  console.log("props", props.users);
+  const timer = setTimeout(() => {
+    localStorage.removeItem("wordcount");
+    localStorage.removeItem("textWord");
+  }, 3000);
+
+  console.log("props", props);
   return (
     <Container
       style={{
         display: "flex",
-        justifyContent: "center",
-        background: "#ececec"
+        justifyContent: "center"
       }}
     >
       <Col md={10} sm={10} xs={12}>
         <div style={mystyle.boxCount}>
           <h2 style={mystyle.h2}>UPLOAD MATERIAL</h2>
 
-          <Button variant="primary">Add File</Button>
-          <Button variant="primary" onClick={handleShow}>
+          <Button style={mystyle.btnCekWord}>Add File</Button>
+          <Button style={mystyle.btnCekWord} onClick={handleShow}>
             Type Text
           </Button>
+
+          {localStorage.getItem("wordcount") !== null ? (
+            <Alert variant="dark">
+              word count : {harga("jumlahKata")}
+              <br />
+              Price : Rp. {harga("harga")}
+            </Alert>
+          ) : (
+            <div></div>
+          )}
         </div>
       </Col>
 
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Enter Free Text</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form style={mystyle.form}>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Apply
-          </Button>
-        </Modal.Footer>
+        <Formik
+          initialValues={{
+            textProject: "vddddd dddddd ddddd ddddddddddd dddddd"
+          }}
+          validate={values => {
+            const errors = {};
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            setSubmitting(false);
+            setShow(false);
+
+            localStorage.setItem("textWord", JSON.stringify(values));
+
+            const wordcount = JSON.parse(localStorage.getItem("wordcount"));
+            console.log("harga", wordcount);
+
+            // let harga = wordcount.hargaTotal;
+            // let jumlahKata = wordcount.jumlahKata;
+
+            props.wordCount(values);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            setFieldValue
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <Modal.Header closeButton>
+                <Modal.Title>Enter Free Text</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form.Group onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    name="textProject"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    // value={inputWord}
+                    value={values.textProject}
+                    style={{ height: "80px", width: "100%" }}
+                  />
+                </Form.Group>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" type="submit">
+                  Apply
+                </Button>
+              </Modal.Footer>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </Container>
   );
 }
 
-export default CheckPriceFromText;
+const mapDispatchToProps = dispatch => {
+  console.log("wordCount", wordCount);
+  console.log("dispatch", dispatch);
+  return {
+    wordCount: data => dispatch(wordCount(data))
+  };
+};
+
+export default withRouter(
+  connect(null, mapDispatchToProps)(CheckPriceFromText)
+);
