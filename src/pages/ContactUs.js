e17a7ea82848ff953e49b8452f28cc644458da3e
Pirman Saputra <pirman0290@gmail.com>
2020-03-05 12:26:07 +0700

    done editing contact css responsive

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -9,10 +9,22 @@ export default function ContactUs() {
         <div>
             <BreadContact />
             <Row gutter={[8, 8]} style={{marginLeft: "0", marginRight: "0"}}>
-                <Col xs={{span:24}} sm={{span:24}} md={{span:8}} lg={{span:8}} xl={{span:6}}>
+                <Col 
+                    xs={{span:24}} 
+                    sm={{span:24}} 
+                    md={{span:8}} 
+                    lg={{span:8}} 
+                    xl={{span:8}}
+                >
                     <Gmap />
                 </Col>
-                <Col xs={{span:24}} sm={{span:24}} md={{span:8}} lg={{span:8}} xl={{span:6}}>
+                <Col 
+                    xs={{span:24}} 
+                    sm={{span:24}} 
+                    md={{span:4}} 
+                    lg={{span:4}} 
+                    xl={{span:4}}
+                >
                     <FormContact />
                 </Col>
             </Row>
